9e25d1c19f712c0f94c792d6b79a63c570f7cd4b
Hao <144909142+lucas6028@users.noreply.github.com>
2025-02-21 01:11:07 +0000

    modify addVocabulary hook

--- hooks/addVocabulary.ts
+++ hooks/addVocabulary.ts
@@ -1,26 +1,9 @@
 import { firestore } from "@/config/firebaseConfig";
+import { Vocabulary } from "@/types/types";
 
-export const addFirestoreVocabulary = async () => {
+export const addFirestoreVocabulary = async (newData: Vocabulary) => {
   try {
-    await firestore().collection("vocabularies").add({
-        id: '1',
-        word: 'Serendipity',
-        definition: 'The occurrence and development of events by chance in a happy or beneficial way',
-        example: 'Finding your dream job while helping a stranger was pure serendipity',
-        level: 'Advanced',
-        dateAdded: '2025-02-17',
-        imagePrompt: 'magical coincidence rainbow butterfly effect',
-        reviewSchedule: [
-          { date: '2025-02-18', completed: false },
-          { date: '2025-02-19', completed: true },
-          { date: '2025-02-20', completed: false },
-          { date: '2025-02-21', completed: false },
-          { date: '2025-02-22', completed: false },
-          { date: '2025-02-23', completed: false },
-        ],
-        lastReviewed: '2024-02-18',
-        reviewHistory: ['2024-02-16', '2024-02-18'],
-    });
+    await firestore().collection("vocabularies").add(newData);
     console.log("Vocabulary added!");
   } catch (error) {
     console.error("Error adding vocabulary:", error);
