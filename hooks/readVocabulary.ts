649a4b245e532f07edda17637e3a06726569a432
Hao <144909142+lucas6028@users.noreply.github.com>
2025-02-23 04:32:11 +0000

    modified print statement of readVocabulary.ts

--- hooks/readVocabulary.ts
+++ hooks/readVocabulary.ts
@@ -4,8 +4,9 @@ export const readFirestoreVocabulary = async () => {
   try {
     const querySnapshot = await firestore().collection("vocabularies").get();
     querySnapshot.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
+      // console.log(doc.id, " => ", doc.data().word);
     });
+    console.log("Vocabulary read!");
     return querySnapshot.docs.map((doc) => doc.data());
   } catch (error) {
     console.error("Error reading vocabulary:", error);
