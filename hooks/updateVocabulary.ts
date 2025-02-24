cf2bd20fbb30c7ecb0a43eec75fcf80fb34dbbe1
Hao <144909142+lucas6028@users.noreply.github.com>
2025-02-24 12:33:59 +0000

    change the title of updateVocabulary

--- hooks/updateVocabulary.ts
+++ hooks/updateVocabulary.ts
@@ -2,7 +2,7 @@ import { firestore } from "@/config/firebaseConfig";
 import { Vocabulary } from "@/types/types";
 import { doc, updateDoc } from "firebase/firestore";
 
-export const updateVocabulary = async (id: string, newData: Vocabulary) => {
+export const updateFirestoreVocabulary = async (id: string, newData: Vocabulary) => {
   try {
     const docRef = doc(firestore, "vocabularies", id);
     await updateDoc(docRef, { ...newData } as any);
