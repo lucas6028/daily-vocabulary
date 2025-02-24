bd288a279326d9c31263accb2e9283a6545cd9fe
Hao <144909142+lucas6028@users.noreply.github.com>
2025-02-24 12:33:46 +0000

    add vocabulary with custom id

--- hooks/addVocabulary.ts
+++ hooks/addVocabulary.ts
@@ -1,10 +1,14 @@
 import { firestore } from "@/config/firebaseConfig";
 import { Vocabulary } from "@/types/types";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, doc, setDoc } from "firebase/firestore";
 
-export const addFirestoreVocabulary = async (newData: Vocabulary) => {
+export const addFirestoreVocabulary = async (newData: Vocabulary, id: string) => {
   try {
-    await addDoc(collection(firestore, "vocabularies"), newData);
+   // Reference to the "vocabularies" collection with the custom document ID
+   const docRef = doc(collection(firestore, "vocabularies"), id);
+    
+   // Set the data to the specified document
+   await setDoc(docRef, newData);
     console.log("Vocabulary added!");
   } catch (error) {
     console.error("Error adding vocabulary:", error);
